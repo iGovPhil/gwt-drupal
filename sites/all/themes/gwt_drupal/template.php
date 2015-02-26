48455b7dc2c975cba8add9528c5ed144dc695882
jeturian <jeturian@gmail.com>
2015-02-26 17:56:23 +0800

    fix syntax error

--- sites/all/themes/gwt_drupal/template.php
+++ sites/all/themes/gwt_drupal/template.php
@@ -701,7 +701,7 @@ function gwt_drupal_preprocess_page(&$variables, $hook) {
   // TODO: check if the auxiliary_menu is available
   $variables['menu_auxiliary_menu'] = '';
   if(menu_load_links('menu-auxiliary-menu')) {
-    $variables['menu_auxiliary_menu'] = menu_load_links('menu-auxiliary-menu')
+    $variables['menu_auxiliary_menu'] = menu_load_links('menu-auxiliary-menu');
   }
   
   $accessibility = array();
