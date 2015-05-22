1e75937a137df4f5e5263a600ff74b1a47738d7c
jeturian <jeturian@gmail.com>
2015-05-22 14:13:12 +0800

    Change the font size of the sidebar headings

--- sites/all/themes/gwt_drupal/templates/block.tpl.php
+++ sites/all/themes/gwt_drupal/templates/block.tpl.php
@@ -11,7 +11,7 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+    <h5<?php print $title_attributes; ?>><?php print $title; ?></h5>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
